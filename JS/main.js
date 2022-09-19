52dbcdfcbb5e1e1a37d5871f171f658104665578
Rokesh Arumugam <74315313+RokeshArumugam@users.noreply.github.com>
2022-09-19 14:40:39 +0100

    Fix bug where contitution wasn't in skills dict

--- JS/main.js
+++ JS/main.js
@@ -66,6 +66,7 @@ const pathnamesForDetailType = {
 const skillsForAbilities = {
 	"strength": ["athletics"],
 	"dexterity": ["acrobatics", "sleightOfHand", "stealth"],
+	"constitution": [],
 	"intelligence": ["arcana", "history", "investigation", "nature", "religion"],
 	"wisdom": ["animalHandling", "insight", "medicine", "perception", "survival"],
 	"charisma": ["deception", "intimidation", "performance", "persuasion"]
